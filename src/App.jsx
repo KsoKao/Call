c44b8c1d9aee266ff7c996780351999a2d0a812b
KsoKaa <kami38720@gmail.com>
2023-04-20 16:51:32 +0900

    call

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import Web3 from "web3";
-import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./web3.config";
-// 웹3 import
+import {
+  CONTRACT_ABI,
+  CONTRACT_ADDRESS,
+  NFT_ABI,
+  NFT_ADDRESS,
+} from "./web3.config";
 
 const web3 = new Web3("https://rpc-mumbai.maticvigil.com");
 const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+const nftContract = new web3.eth.Contract(NFT_ABI, NFT_ADDRESS);
 
 function App() {
   const [account, setAccount] = useState("");
@@ -14,7 +19,6 @@ function App() {
     try {
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
-        // 키값 : 요청값
       });
 
       setAccount(accounts[0]);
@@ -22,11 +26,9 @@ function App() {
       console.error(error);
     }
   };
-
-  const onClickLogout = () => {
+  const onClickLogOut = () => {
     setAccount("");
   };
-
   const onClickBalance = async () => {
     try {
       if (!account || !contract) return;
@@ -34,6 +36,23 @@ function App() {
       const balance = await contract.methods.balanceOf(account).call();
 
       setMyBalance(web3.utils.fromWei(balance));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const onClickMint = async () => {
+    try {
+      if (!account) return;
+
+      const result = await nftContract.methods
+        .mintNft(
+          "https://gateway.pinata.cloud/ipfs/QmUMC6wtFsQTbX4SR1Ngx9Ze1SdU7RBWiTRtTwRCLEEJja"
+        )
+        .send({
+          from: account,
+        });
+
+      console.log(result);
     } catch (error) {
       console.error(error);
     }
@@ -46,14 +65,19 @@ function App() {
           <div className="text-main font-semibold text-2xl">
             {account.substring(0, 4)}...
             {account.substring(account.length - 4)}
-            <button className="ml-4 btn-style" onClick={onClickLogout}>
+            <button className="ml-4 btn-style" onClick={onClickLogOut}>
               로그아웃
             </button>
           </div>
           <div className="flex items-center mt-4">
-            {myBalance && <div>{myBalance} TT</div>}
+            {myBalance && <div>{myBalance} tMatic</div>}
             <button className="ml-2 btn-style" onClick={onClickBalance}>
               잔액 조회
+            </button>
+          </div>
+          <div className="flex items-center mt-4">
+            <button className="ml-2 btn-style" onClick={onClickMint}>
+              민팅
             </button>
           </div>
         </div>
